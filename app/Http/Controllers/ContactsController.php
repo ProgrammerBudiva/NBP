9d2fbb4215b05bfabe83856a50190d8d49402506
andrey <19ofis96@mail.ru>
2017-08-31 12:57:20 +0300

    email in contacts message

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -38,8 +38,8 @@ class ContactsController extends Controller
 
 
         Mail::send('mails.new_question', ['phone' => $phone, 'diller' => $diller, 'name' => $name, 'email' => $email, 'text' => $message], function ($message) {
-            $message->from('info@nbp.net.ua', 'BP');
-            $message->to('19ofis96@mail.ru');
+            $message->from('info@bpcan.in.ua', 'BP');
+            $message->to('info@bpcan.in.ua');
         });
 
         $returnHTML = view('layouts.popup')->render();
