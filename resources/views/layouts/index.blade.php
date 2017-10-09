949bce00f21b6b096b2ea0ac5d72f86fe917f7b9
andrey <19ofis96@mail.ru>
2017-10-09 10:13:17 +0300

    email in contacts message

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -9,6 +9,7 @@
         <meta name="format-detection" content="telephone=no" />
         <meta name="keywords" content="Construction, Renovation" />
         <meta name="description" content="Responsive Construction Renovation Template" />
+        <meta name="google-site-verification" content="BDvAFkneNm1dUXcYSzWa0ZapyA5akgjG1ehN0iQj-C4" />
             <!--slider revolution-->
         <link rel="stylesheet" type="text/css" href="{{ asset('plugins/rs-plugin/css/settings.css')}}" media="screen" />
         <!--style-->
@@ -49,6 +50,48 @@
 
         <script>    var BASE_URL = "{{ url('/') }}" </script>
         @include('footer')
+
+        <!— Global Site Tag (gtag.js) - Google Analytics —>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107765565-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-107765565-1');
+        </script>
+
+        <!— Yandex.Metrika counter —>
+        <script type="text/javascript" >
+            (function (d, w, c) {
+                (w[c] = w[c] || []).push(function() {
+                    try {
+                        w.yaCounter46209888 = new Ya.Metrika({
+                            id:46209888,
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true,
+                            ecommerce:"dataLayer"
+                        });
+                    } catch(e) { }
+                });
+
+                var n = d.getElementsByTagName("script")[0],
+                    s = d.createElement("script"),
+                    f = function () { n.parentNode.insertBefore(s, n); };
+                s.type = "text/javascript";
+                s.async = true;
+                s.src = "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/watch.js";
+
+                if (w.opera == "[object Opera]") {
+                    d.addEventListener("DOMContentLoaded", f, false);
+                } else { f(); }
+            })(document, window, "yandex_metrika_callbacks");
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/46209888" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!— /Yandex.Metrika counter —>
+
     </div>
     </body>
 
